839dae9d1e2893adf7570cc34e3613218daaba0e
Gokhan <engokhangok@gmail.com>
2023-09-07 19:13:33 +0300

    Added listing of categories in API

--- RealEstateDapperAPI/Program.cs
+++ RealEstateDapperAPI/Program.cs
@@ -1,7 +1,11 @@
+using RealEstateDapperAPI.Models.DapperContext;
+using RealEstateDapperAPI.Repositories.CategoryRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddTransient<Context>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
