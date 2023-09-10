0a72b70cc580e618ba30a15116c4f585c48b9c23
Gokhan <engokhangok@gmail.com>
2023-09-10 12:47:07 +0300

    Home page divided into partials

---
+++ RealEstateDapperUI/ViewComponents/HomePage/_DefaultOurClientsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateDapperUI.ViewComponents.HomePage
+{
+    public class _DefaultOurClientsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstateDapperUI/ViewComponents/Layout/_NavbarViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateDapperUI.ViewComponents.Layout
+{
+    public class _NavbarViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
