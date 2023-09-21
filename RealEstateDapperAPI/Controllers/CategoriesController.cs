8d398194fab29119f1a64451b761cb7511eec94c
Gokhan <engokhangok@gmail.com>
2023-09-21 14:31:58 +0300

    Added Delete and Update Category With API Consumed

--- RealEstateDapperAPI/Controllers/CategoriesController.cs
+++ RealEstateDapperAPI/Controllers/CategoriesController.cs
@@ -27,7 +27,7 @@ namespace RealEstateDapperAPI.Controllers
             _categoryRepository.CreateCategory(createCategoryDto);
             return Ok("Kategori Başarılı Bir Şekilde Eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCategory(int id)
         {
             _categoryRepository.DeleteCategory(id);
